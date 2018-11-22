125d53ee2d8fab65ec5e91908ce9c2d3c647b7b8
Benoît <benoit.puech@yahoo.fr>
2018-11-22 11:31:10 +0100

    functions headers

---
+++ functions.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONS_H
+
+#define FUNCTIONS_H
+
+void choice();
+
+void generateNumber(int choice, int min, int max);
+
+void game(int mysteryNumber, int min, int max);
+
+int strokesCount(int *strokes);
+
+int goon(int *startAgain);
+
+#endif
