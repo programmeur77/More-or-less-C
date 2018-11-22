7f9880e9802825c78ad76af7c6654ca89b2b4be1
Benoît <benoit.puech@yahoo.fr>
2018-11-22 22:51:56 +0100

    Headers updates

--- functions.h
+++ functions.h
@@ -4,15 +4,15 @@
 
 void choiceLevel();
 
-void generateNumber(int choice, int min, int max);
+void generateNumber(int choice, int min, int max, int level);
 
-void game(int mysteryNumber, int min, int max);
+void game(int mysteryNumber, int min, int max,int level);
 
 int strokesCount(int *strokes);
 
-void checkRecord(int strokes);
+void checkRecord(int strokes, int level);
 
-void saveRecord(int *record);
+void saveRecord(int *record, int level);
 
 int goon(int *startAgain);
 
